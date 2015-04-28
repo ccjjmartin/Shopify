681636f51f4d2b34cfd7b29fb398b854921318f3
donutdan4114 <donutdan4114@gmail.com>
2015-04-28 12:48:48 -0400

    Fixes #2478953: Setup JS to POST to backend first for message, then submit the form.

--- shopify.js
+++ shopify.js
@@ -8,11 +8,17 @@
   /**
    * Display an "Added to cart" message by sending a POST request to the backend.
    */
-  Drupal.shopify.display_add_to_cart_message = function (el) {
-    var $el = $(el);
-    $.post('/shopify/added-to-cart', {
-      product_id: $el.data('product-id'),
-      variant_id: $el.data('variant-id')
+  Drupal.shopify.display_add_to_cart_message = function () {
+    var $forms = $('form.shopify-add-to-cart-form');
+    $forms.unbind('submit').submit(function (e) {
+      var $form = $(this);
+      e.preventDefault();
+      $.post('/shopify/added-to-cart', {
+        product_id: $form.data('product-id'),
+        variant_id: $form.data('variant-id')
+      }, function (data) {
+        $form.get(0).submit();
+      });
     });
   };
 
@@ -20,6 +26,7 @@
     attach: function (context, settings) {
       Drupal.shopify.ctx = context;
       Drupal.shopify.settings = settings;
+      Drupal.shopify.display_add_to_cart_message();
     }
   }
 
