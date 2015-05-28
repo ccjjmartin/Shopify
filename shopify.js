3a8f6175c0b6de2e9f2266fcc9ca0c2f01604c0b
bazzmann <bazzmann@383245.no-reply.drupal.org>
2015-05-28 17:18:48 -0400

    Issue #2493637 by bazzmann: Base path on Javascript add-to-cart function

--- shopify.js
+++ shopify.js
@@ -17,7 +17,7 @@
     $forms.unbind('submit').submit(function (e) {
       var $form = $(this);
       e.preventDefault();
-      $.post('/shopify/added-to-cart', {
+      $.post(Drupal.settings.basePath+'/shopify/added-to-cart', {
         product_id: $form.data('product-id'),
         variant_id: $form.data('variant-id')
       }, function (data) {
