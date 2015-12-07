66272970ee59dabba10c1e3ee5b987aa3deca762
Daniel Pepin <donutdan4114@gmail.com>
2015-12-07 12:16:14 -0500

    Issue #2536510: Setup overview admin page to display store info.

--- src/Form/ShopifySettingsAdminForm.php
+++ src/Form/ShopifySettingsAdminForm.php
@@ -10,6 +10,7 @@ namespace Drupal\shopify\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 /**
  * Class ShopifySettingsAdminForm.
@@ -17,15 +18,6 @@ use Drupal\Core\Form\FormStateInterface;
  * @package Drupal\shopify\Form
  */
 class ShopifySettingsAdminForm extends FormBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEditableConfigNames() {
-    return [
-      'shopify.shopifysettingsadmin'
-    ];
-  }
 
   /**
    * {@inheritdoc}
@@ -38,7 +30,40 @@ class ShopifySettingsAdminForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('shopify.shopifysettingsadmin');
+    $client = shopify_api_client();
+    try {
+      $info = $client->getShopInfo();
+    } catch (\Exception $e) {
+      // Error connecting to the store.
+      drupal_set_message(t('Could not connect to the Shopify store.'), 'error');
+      return [];
+    }
+    $store_info = array(
+      'My Store Admin' => \Drupal::l($info->domain, Url::fromUri('https://' . $info->domain . '/admin', ['attributes' => ['target' => '_blank']])),
+      'Owned By' => $info->shop_owner . ' &lt;<a href="mailto:' . $info->email . '">' . $info->email . '</a>&gt;',
+      'Address' => $info->address1,
+      'City' => $info->city,
+      'State/Province' => $info->province,
+      'Zip' => $info->zip,
+    );
+    foreach ($store_info as $label => $data) {
+      $form[$label] = [
+        '#type' => 'item',
+        '#title' => $label,
+        '#markup' => $data,
+      ];
+    }
+    $form['all_info'] = [
+      '#type' => 'details',
+      '#title' => t('More info'),
+    ];
+    foreach ($info as $label => $data) {
+      $form['all_info'][$label] = [
+        '#type' => 'item',
+        '#title' => $label,
+        '#markup' => $data ?: '[EMPTY]',
+      ];
+    }
     return $form;
   }
 
@@ -54,9 +79,6 @@ class ShopifySettingsAdminForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
-    $this->config('shopify.shopifysettingsadmin')
-      ->save();
   }
 
 }
