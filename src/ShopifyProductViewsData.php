8aef274e4df0b6a82557b6353202bc60d382fa9a
Daniel Pepin <donutdan4114@gmail.com>
2015-12-14 17:45:46 -0500

    Started views integration. Not working yet.

--- src/ShopifyProductViewsData.php
+++ src/ShopifyProductViewsData.php
@@ -19,19 +19,26 @@ class ShopifyProductViewsData extends EntityViewsData {
   public function getViewsData() {
     $data = parent::getViewsData();
     // @todo: No idea what I'm doing.
-    // Something matching this setup: /core/modules/taxonomy/src/TermViewsData.php
 
-//    $data['shopify_product']['shopify_product_tags']['entity field'] = 'shopify_product_tags';
-//    $data['shopify_product']['shopify_product_tags']['help'] = t('');
-//    $data['shopify_product']['shopify_product_tags']['field']['id'] = 'field';
-//    $data['shopify_product']['shopify_product_tags']['filter']['id'] = 'numeric';
-//    $data['shopify_product']['shopify_product_tags']['sort']['id'] = 'standard';
-//    $data['shopify_product']['shopify_product_tags']['title'] = t('Shopify Product Tags');
-//    $data['shopify_product']['shopify_product_tags']['relationship']['base'] = 'shopify_product__tags';
-//    $data['shopify_product']['shopify_product_tags']['relationship']['base field'] = 'entity_id';
-//    $data['shopify_product']['shopify_product_tags']['relationship']['id'] = 'standard';
-//    $data['shopify_product']['shopify_product_tags']['relationship']['label'] = t('Shopify Product Tags');
-//    $data['shopify_product']['shopify_product_tags']['relationship']['title'] = t('Shopify Product Tags');
+    $data['shopify_product']['tags']['title'] = t('Shopify Product Tags');
+    $data['shopify_product']['tags']['help'] = t('Select based on tagged term ID.');
+    $data['shopify_product']['tags']['entity field'] = 'tags';
+    $data['shopify_product']['tags']['field']['id'] = 'field';
+    $data['shopify_product']['tags']['filter']['field'] = 'tags_target_id';
+    $data['shopify_product']['tags']['filter']['id'] = 'numeric';
+//    $data['shopify_product']['tags']['table']['base'] = 'shopify_product__tags';
+    $data['shopify_product']['tags']['table']['join']['shopify_product__tags'] = [
+      'left_field' => 'entity_id',
+      'field' => 'id',
+    ];
+    $data['shopify_product']['tags']['relationship'] = [
+      'base' => 'shopify_product__tags',
+      'base field' => 'entity_id',
+      'id' => 'numeric',
+      'label' => t('Tag'),
+      'title' => t('Tag'),
+    ];
+
     return $data;
   }
 
