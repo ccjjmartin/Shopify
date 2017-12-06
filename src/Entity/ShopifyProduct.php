5ab945567e878de2489a5dad1bd7d72b3a303d2a
stefanos.petrakis <stefanos.petrakis@2573064.no-reply.drupal.org>
2017-12-06 15:03:44 +0200

    Issue #2838157 by stefanos.petrakis: Make Shopify Product translatable

--- src/Entity/ShopifyProduct.php
+++ src/Entity/ShopifyProduct.php
@@ -40,10 +40,13 @@ use Drupal\user\UserInterface;
  *     "access" = "Drupal\shopify\ShopifyProductAccessControlHandler",
  *   },
  *   base_table = "shopify_product",
+ *   data_table = "shopify_product_field_data",
  *   admin_permission = "administer ShopifyProduct entity",
+ *   translatable = TRUE,
  *   entity_keys = {
  *     "id" = "id",
  *     "label" = "title",
+ *     "langcode" = "langcode",
  *     "uuid" = "uuid"
  *   },
  *   links = {
