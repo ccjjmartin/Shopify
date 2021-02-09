8c640620eb258a11c39990ff434f713baac31acb
bobbygryzynger <bobby.gryzynger@3311649.no-reply.drupal.org>
2021-02-09 18:56:29 +0000

    Issue #3197423 by bobbygryzynger: Add test coverage for API sync

--- .gitlab-ci/build.php
+++ .gitlab-ci/build.php
@@ -2,39 +2,9 @@
 
 /**
  * @file
- * You can use this file to run extra Robo build tasks.
+ * Custom GitlabCI build steps.
  *
- * See RoboFile.php and https://robo.li/ documentation to run a task.
- *
- * For CI_TYPE='project', this file is executed directly during the Build job
- * script after the regular composer install.
- *
- * For CI_TYPE='module', this file is executed on each 'before_script' part of
- * jobs.
- *
- * It's important to have any action relative to the docRoot or webRoot as we are
- * not working from the CI_PROJECT_DIR.
- *
- * Examples:
- *
- * $this->say("This will be run in Build script!");
- *
- * $this->taskGulpRun()
- *   ->dir($this->webRoot . 'themes/my_theme_with_gulp_task')
- *   ->run();
- *
- * $this->taskComposerRequire()
- *   ->noInteraction()
- *   ->noAnsi()
- *   ->workingDir($this->docRoot);
- *   ->dependency('drupal/webform', '^5.13')
- *   ->run();
- *
- * Or shortcut method in the RoboFile.php with this project:
- * $this->composerRequire()
- *  ->dependency('drupal/webform', '^5.13')
- *  ->run();
- *
+ * @see https://mog33.gitlab.io/gitlab-ci-drupal/advanced-usage/#custom-build
  */
 
 // Add module dependencies. This must match the module's composer.json.
